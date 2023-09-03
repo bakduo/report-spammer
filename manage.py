791107d7952c5e794080779424d51daa95b04a1e
bakduo <453619+bakduo@users.noreply.github.com>
2023-09-02 21:47:00 -0300

    update app and fixit version 1.0.1

--- manage.py
+++ manage.py
@@ -6,10 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messageadmin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authdj.settings')
     try:
         from django.core.management import execute_from_command_line
-        
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -17,7 +16,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    
 
 
 if __name__ == '__main__':
